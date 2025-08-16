9c568aa160ff1a86dabf910d4cd1d74dabb7345e
yamilaguirre <yamil.aguirre.lopezgmail.com>
2025-08-15 21:39:55 -0400

    create CorsConfig

---
+++ src/main/java/com/pharmacy/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.pharmacy.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration // Marca esta clase como una fuente de definiciones de beans para Spring
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
